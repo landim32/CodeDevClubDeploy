8113b1b534f62d0708b8bb620d26a12be9f963ff
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-11-25 00:02:23 -0300

    first commit

---
+++ BEHR/env.js
@@ -0,0 +1,11 @@
+window.env = {
+  "NODE_PATH": "./src",
+  "REACT_APP_PRODUCTION": "0",
+  "PORT": "80",
+  "BEHR_TOKEN_CONTRACT": "0xb3b7ab83A2c18e0EFC14de551f7255694E303eCd",
+  "BEHR_CONTRACT": "0x8430041918b5942750d509282F9c1ffea073967E",
+  "REACT_APP_NETWORK": "97",
+  "REACT_APP_CHAIN_URL": "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  "REACT_APP_CHAIN_NAME": "Smart Chain - Testnet",
+  "REACT_APP_CHAIN_EXPLORER": "https://testnet.bscscan.com"
+};
---
+++ CodeDevClub/env.js
@@ -0,0 +1,9 @@
+window.env = {
+  "NODE_PATH": "./src",
+  "REACT_APP_PRODUCTION": "0",
+  "PORT": "80",
+  "REACT_APP_NETWORK": "97",
+  "REACT_APP_CHAIN_URL": "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  "REACT_APP_CHAIN_NAME": "Smart Chain - Testnet",
+  "REACT_APP_CHAIN_EXPLORER": "https://testnet.bscscan.com"
+};
